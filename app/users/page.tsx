517bea05bd2eb7c8a38e78e8f633e2e152c3f02f
s4remi <s4remi@gmail.com>
2023-12-24 22:53:17 -0800

    modify the globals css file, change the cach to not storage means continue refreshing

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,14 +7,15 @@ interface User {
 const UsersPage = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
     // no caching when we want to have a fresh data
-    // cache: "no-store",
+    cache: "no-store",
     // caching after certain amount of time in this case 10s
-    next: { revalidate: 10 },
+    //next: { revalidate: 10 },
   });
   const users: User[] = await res.json();
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
