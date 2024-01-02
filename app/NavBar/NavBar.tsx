1e84e3d0a6a1ca83ed9c64ae95d90bafa2f94618
s4remi <s4remi@gmail.com>
2024-01-01 21:25:24 -0800

    style the active link in the navbar

--- app/NavBar/NavBar.tsx
+++ app/NavBar/NavBar.tsx
@@ -1,9 +1,13 @@
-import { link } from "fs";
+"use client";
 import Link from "next/link";
 import React from "react";
 import { VscDebugConsole } from "react-icons/vsc";
+import classNames from "classnames";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
+  //using usePath hook for knowing what link is active
+  const currentPath = usePathname();
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -18,7 +22,11 @@ const NavBar = () => {
           <Link
             key={item.href}
             href={item.href}
-            className="text-zinc-700 hover:text-sky-700"
+            className={classNames({
+              "text-sky-900": item.href === currentPath,
+              "text-zinc-700 ": item.href !== currentPath,
+              "hover:text-orange-300": true,
+            })}
           >
             {item.label}
           </Link>
